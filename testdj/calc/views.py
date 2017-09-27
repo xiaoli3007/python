6ab33336918508e25e3f743068220d9fc78d12d9
sunli <sunli3007@qq.com>
2017-09-27 21:52:20 +0800

    啊

--- testdj/calc/views.py
+++ testdj/calc/views.py
@@ -48,9 +48,12 @@ def videolist(request):
 
     columns = []
     for item in column.object_list:
-        s = '%s%s_poster.jpg' % (MEDIA_IMAGE_URL, item.filepath)
+
+        jpg = '%s%s_poster.jpg' % (MEDIA_IMAGE_URL, item.filepath)
+        png = '%s%s_tposter_0x0_0.png' % (MEDIA_IMAGE_URL, item.filepath)
         smfilepath = '%s%s' % (MEDIA_IMAGE_URL, item.filepath)
-        if not os.path.exists(s):
+
+        if not os.path.exists(jpg) and not os.path.exists(png):
            ret = functhumb(smfilepath, os.path.dirname(smfilepath))
         columns.append(item)
     column.object_list = columns
